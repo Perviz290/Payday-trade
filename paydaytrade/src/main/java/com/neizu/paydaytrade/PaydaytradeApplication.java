8fe5027f37ad503caae0dafbb1873d7e68102ae8
Perviz290 <112194956+Perviz290@users.noreply.github.com>
2023-05-02 01:34:57 -0700

    ...

---
+++ paydaytrade/src/main/java/com/neizu/paydaytrade/PaydaytradeApplication.java
@@ -0,0 +1,13 @@
+package com.neizu.paydaytrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaydaytradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaydaytradeApplication.class, args);
+	}
+
+}
---
+++ paydaytrade/src/test/java/com/neizu/paydaytrade/PaydaytradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neizu.paydaytrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaydaytradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
